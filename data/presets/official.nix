3657e72e053a22096e66e24719124c7e31120cae
lunik1 <ch.gpg@themaw.xyz>
2023-02-04 13:32:14 +0000

    presets.official: use recursive set

--- data/presets/official.nix
+++ data/presets/official.nix
@@ -1,9 +1,9 @@
-{
+rec {
   backgroundColor = "#ffffff";
   color0 = "#7ebae4";
   color1 = "#5277c3";
-  color2 = "#7ebae4";
-  color3 = "#5277c3";
-  color4 = "#7ebae4";
-  color5 = "#5277c3";
+  color2 = color0;
+  color3 = color1;
+  color4 = color0;
+  color5 = color1;
 }
