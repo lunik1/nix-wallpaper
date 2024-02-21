29c511a287896acba639764ec0ba1d87117902b5
lunik1 <ch.gpg@themaw.xyz>
2024-02-21 21:23:45 +0000

    add generate-previews.sh script

---
+++ scripts/generate-previews.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+basename() {
+    dir=${1%"${1##*[!/]}"}
+    dir=${dir##*/}
+    dir=${dir%"$2"}
+    printf '%s\n' "${dir:-/}"
+}
+
+dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+out=$(realpath "${dir}"/../previews)
+
+if [ -e "${out}" ]; then
+  printf "Error: %s already exists\n" "${out}" >&2
+  exit 1
+fi
+
+mkdir -p "${out}"
+
+for f in "${dir}"/../data/presets/*.nix; do
+  theme=$(basename "$f" ".nix")
+  printf "Generating preview for %s\n" "$theme"
+
+  nix build \
+    --impure \
+    --expr "(builtins.getFlake \"github:lunik1/nix-wallpaper\").packages.\${builtins.currentSystem}.default.override { width = 150; height = 150; logoSize = 80; preset = \"${theme}\"; }"
+  install -m644 result/share/wallpapers/nixos-wallpaper.png "${out}/${theme}.png"
+done
+
+rm -rf result
