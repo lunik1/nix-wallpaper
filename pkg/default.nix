c54e6afa3eb12d1818a6a726dfaae93ef645ff02
Anton Mosich <anton@mosich.at>
2024-10-12 15:54:54 +0200

    Allow transparent background

--- pkg/default.nix
+++ pkg/default.nix
@@ -16,7 +16,13 @@
 
 let
   isNixFile = file: type: (lib.hasSuffix ".nix" file && type == "regular");
-  isColor = str: builtins.isString str && (builtins.isList (builtins.match "^#[0-9a-fA-F]{6}$" str));
+  isColor = str: builtins.isString str && (builtins.isList (builtins.match "^#[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$" str));
+  # Adds full opacity if none is provided
+  opacityHex = str: builtins.substring 7 2 (str + "ff");
+  opacityDec = str: lib.fromHexString (opacityHex str) / 255.0;
+  opacity = name: value: if name == "backgroundColor" then opacityHex value else opacityDec value;
+  opacityName = name: if name == "backgroundColor" then "backgroundOpacity" else "opacity" + (builtins.substring 5 1 name);
+  color = str: builtins.substring 0 7 str;
 in
 
 assert builtins.isInt width && width > 0;
@@ -28,9 +34,9 @@ assert lib.assertMsg
     (lib.filterAttrs isNixFile (builtins.readDir ../data/presets)))
   "unknown preset \"${preset}\"";
 assert lib.assertMsg (backgroundColor == null || isColor backgroundColor)
-  "backgroundColor should be a 6-digit hex code";
+  "backgroundColor should be a 6 or 8-digit hex code";
 assert lib.assertMsg (builtins.all isColor (lib.attrValues logoColors))
-  "logoColors should contain 6-digit hex codes";
+  "logoColors should contain 6 or 8-digit hex codes";
 assert lib.assertMsg
   (builtins.all
     (str: builtins.isList (builtins.match "^color[0-5]$" str))
@@ -39,14 +45,23 @@ assert lib.assertMsg
 assert builtins.isBool widdershins;
 
 let
-  colorscheme = import ../data/presets/${preset}.nix
+  finalColors = import ../data/presets/${preset}.nix
     // logoColors //
     lib.optionalAttrs (backgroundColor != null) { inherit backgroundColor; };
+  colorscheme = lib.concatMapAttrs
+    (
+      name: value: {
+        ${name} = color value;
+        ${opacityName name} = opacity name value;
+      }
+    )
+    finalColors;
 in
 runCommandLocal "nix-wallpaper"
 rec {
   inherit width height;
-  inherit (colorscheme) color0 color1 color2 color3 color4 color5 backgroundColor;
+  inherit (colorscheme) color0 color1 color2 color3 color4 color5 backgroundColor
+    opacity0 opacity1 opacity2 opacity3 opacity4 opacity5 backgroundOpacity;
   buildInputs = [ imagemagick ];
   density = 1200;
   # 72 is the default density
@@ -59,7 +74,7 @@ rec {
   convert \
     -resize ''${scale}% \
     -density $density \
-    -background $backgroundColor \
+    -background ''${backgroundColor}''${backgroundOpacity} \
     -gravity center \
     -extent ''${width}x''${height} \
     $flop \
