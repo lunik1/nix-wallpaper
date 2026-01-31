f658947fdc9ab85be9003deb26632a464bd41e5d
lunik1 <ch.gpg@themaw.xyz>
2026-01-31 01:05:48 +0000

    improve generate-previews.sh script
    Now defers all the job scheduling work to nix.

--- scripts/generate-previews.sh
+++ scripts/generate-previews.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env sh
 
-set -eu
-
-basename() {
-    dir=${1%"${1##*[!/]}"}
-    dir=${dir##*/}
-    dir=${dir%"$2"}
-    printf '%s\n' "${dir:-/}"
-}
+set -eu pipefail
 
 dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 out=$(realpath "${dir}"/../previews)
@@ -19,14 +12,39 @@ fi
 
 mkdir -p "${out}"
 
-for f in "${dir}"/../data/presets/*.nix; do
-  theme=$(basename "$f" ".nix")
-  printf "Generating preview for %s\n" "$theme"
+printf "Generating all previews...\n"
 
-  nix build \
-    --impure \
-    --expr "(builtins.getFlake \"${dir}/..\").packages.\${builtins.currentSystem}.default.override { width = 150; height = 150; logoSize = 80; preset = \"${theme}\"; }"
-  install -m644 result/share/wallpapers/nixos-wallpaper.png "${out}/${theme}.png"
-done
+nix build \
+  --impure \
+  --expr "
+let
+  flake = builtins.getFlake \"${dir}/..\";
+  system = builtins.currentSystem;
+  pkgs = flake.inputs.nixpkgs.legacyPackages.\${system};
+  lib = pkgs.lib;
 
-rm -rf result
+  presetsDir = \"${dir}/../data/presets\";
+  presets = lib.filterAttrs (n: v: v == \"regular\" && lib.hasSuffix \".nix\" n) (builtins.readDir presetsDir);
+  presetNames = lib.mapAttrsToList (n: v: lib.removeSuffix \".nix\" n) presets;
+
+  mkPreview = theme:
+    let
+      pkg = flake.packages.\${system}.default.override {
+        width = 150;
+        height = 150;
+        logoSize = 80;
+        preset = theme;
+      };
+    in
+      {
+        name = \"\${theme}.png\";
+        path = \"\${pkg}/share/wallpapers/nixos-wallpaper.png\";
+      };
+
+  entries = map mkPreview presetNames;
+in
+  pkgs.linkFarm \"previews\" entries
+"
+
+install -m644 result/*.png "${out}/"
+rm result
